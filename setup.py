e53387c94eab8c00a7636bfae0cc9ffd6bcb029b
Oskar Vuola <oskar.vuola@labra.ai>
2022-06-08 15:48:08 +0300

    Add more setup info

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, Extension
 # Compile *mysum.cpp* into a shared library 
 setup(
     name='pyfpng',
+    version='0.1',
+    description='Python bindings for fpng',
+    author='Oskar Vuola',
     packages=['pyfpng'],
     ext_modules=[Extension('pyfpng.libpyfpng', ['src/fpng_py.cpp', 'fpng/src/fpng.cpp'], include_dirs=['fpng/src/'], extra_compile_args=['-msse4.1', '-mpclmul', '-fno-strict-aliasing'])]
 )
