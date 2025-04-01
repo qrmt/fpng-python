1079235b7a899cab01e11e33d82c46b4bc70eaea
Oskar Vuola <oskar.vuola@labra.ai>
2025-04-01 12:17:17 +0300

    Add pyproject.toml to fix build on Python 3.12>=

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup, Extension
-
 import platform
 import numpy
 
-# Setup extension compile arguments
-extra_compile_args = ['-fno-strict-aliasing']
-# If on x86_64, enable SSE
-if platform.processor() == 'x86_64':
-    extra_compile_args += ['-msse4.1', '-mpclmul']
+# Compiler flags
+extra_compile_args = ["-fno-strict-aliasing"]
+if platform.processor() == "x86_64":
+    extra_compile_args += ["-msse4.1", "-mpclmul"]
 
+# Define the extension module
+ext_modules = [
+    Extension(
+        "pyfpng",
+        sources=["src/fpng-python.cpp", "fpng/src/fpng.cpp"],
+        include_dirs=["fpng/src/", numpy.get_include()],
+        extra_compile_args=extra_compile_args,
+    )
+]
+
+# Setup function
 setup(
-    name='fpng-python',
-    version='0.1',
-    description='Python bindings for fpng',
-    author='Oskar Vuola',
-    ext_modules=[Extension('pyfpng', ['src/fpng-python.cpp', 'fpng/src/fpng.cpp'], include_dirs=['fpng/src/', numpy.get_include()], extra_compile_args=extra_compile_args)],
-    install_requires=["numpy>=1.20.0"]
+    ext_modules=ext_modules,
 )
